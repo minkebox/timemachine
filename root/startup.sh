931157f66d3027225c05fb554f2146bc1984b267
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-03-14 00:52:03 -0700

    Set reported disk size

--- root/startup.sh
+++ root/startup.sh
@@ -4,6 +4,10 @@ trap "killall sleep smbd nmbd; exit" TERM INT
 
 mkdir -p /backups
 
+if [ "${MAXSIZE}" != "" ]; then
+  echo "max disk size = $(expr ${MAXSIZE} / 1049600)" >> /etc/samba/extra.conf
+fi
+
 /usr/sbin/nmbd -D
 /usr/sbin/smbd -D
 
