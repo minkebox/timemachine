ed22361c038e89776d33e758b40b5b54c50a7897
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-03-14 01:00:19 -0700

    Escape multiply

--- root/startup.sh
+++ root/startup.sh
@@ -5,7 +5,7 @@ trap "killall sleep smbd nmbd; exit" TERM INT
 mkdir -p /backups
 
 if [ "${MAXSIZE}" != "" ]; then
-  echo "max disk size = $(expr ${MAXSIZE} * 1049600)" >> /etc/samba/extra.conf
+  echo "max disk size = $(expr ${MAXSIZE} \* 1049600)" >> /etc/samba/extra.conf
 fi
 
 /usr/sbin/nmbd -D
