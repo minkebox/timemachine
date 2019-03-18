d415285e1980422f95ef4481e96863dd81bf925c
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-03-18 13:36:44 -0700

    Add optional user/passwd

--- root/startup.sh
+++ root/startup.sh
@@ -4,9 +4,26 @@ trap "killall sleep smbd nmbd; exit" TERM INT
 
 mkdir -p /backups
 
+cp /dev/null /etc/samba/fruit.conf
 if [ "${MAXSIZE}" != "" ]; then
-  echo "fruit:time machine max size = ${MAXSIZE}" > /etc/samba/fruit.conf
+  echo "fruit:time machine max size = ${MAXSIZE}" >> /etc/samba/fruit.conf
 fi
+
+GUEST=yes
+if [ "${SAMBA_USERNAME}" != "" ]; then
+  GUEST=no
+  adduser -D -s /sbin/nologin ${SAMBA_USERNAME}
+  if [ "${SAMBA_PASSWORD}" = "" ]; then
+    passwd -u ${SAMBA_USERNAME}
+    (echo ; echo) | smbpasswd -s -a ${SAMBA_USERNAME}
+    smbpasswd -n ${SAMBA_USERNAME}
+  else
+    echo ${SAMBA_USERNAME}:${SAMBA_PASSWORD} | chpasswd > /dev/null
+    (echo ${SAMBA_PASSWORD} ; echo ${SAMBA_PASSWORD}) | smbpasswd -s -a ${SAMBA_USERNAME}
+  fi
+  smbpasswd -e ${SAMBA_USERNAME}
+fi
+echo "guest ok = ${GUEST}" >> /etc/samba/fruit.conf
 
 /usr/sbin/nmbd -D
 /usr/sbin/smbd -D
